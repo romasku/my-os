fb64808bd8449ec3aadecf099b95af3349ca6776
Roman Skurikhin <roman.skurikhin@cruxlab.com>
2019-08-21 21:26:46 +0300

    Improve qemu runner script

--- tools/run-qemu.sh
+++ tools/run-qemu.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
-qemu-system-x86_64 -monitor stdio -kernel ../cmake-build-debug/kernel.bin
+PATH_TO_KERNEL="../cmake-build-debug/kernel.bin"
+if ! test -f "$PATH_TO_KERNEL"; then
+    PATH_TO_KERNEL="cmake-build-debug/kernel.bin"
+fi
+if ! test -f "$PATH_TO_KERNEL"; then
+    echo "Cannot find 'kernel.bin' file."
+    echo "Please check that kernel build was successfull."
+    echo "Also, note that you can run this script only from project root directory or ./tools directory."
+    exit 1
+fi
+qemu-system-x86_64 -monitor stdio -kernel $PATH_TO_KERNEL
 
 
